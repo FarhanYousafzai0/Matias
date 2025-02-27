08b138d7bca84620e2de6c38c17d09c92c529231
Farhan Yousafzai <coderwithferry@gmail.com>
2025-02-27 20:24:20 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from './Components/Nav'
 import Hero from './Components/Hero'
 const App = () => {
   return (
-    <div className='relative w-screen h-screen overflow-hidden bg-black  text-white'>
+    <div className='relative w-full h-full  bg-black  text-white'>
     <Nav/>
     <Hero/>
     </div>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,47 +1,62 @@
-import React, { useEffect, useRef } from 'react'
-import { FaArrowLeftLong, FaLocationArrow } from 'react-icons/fa6'
-import {gsap} from 'gsap'
-import { useGSAP } from '@gsap/react'
+import React, { useRef } from "react";
+import { FaLocationArrow } from "react-icons/fa6";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
+  const textRef = useRef(null);
 
-    const textRef = useRef()
+  useGSAP(() => {
+    const letters = textRef.current.children;
+    gsap.to(letters, {
+      color: "#C4EC1D",
+      stagger: 0.2,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
+      duration: 0.3,
+      delay: 0.5,
+    });
+  }, []);
 
-    useEffect(() => {
-        const letters = textRef.current.children;
-        gsap.to(letters, {
-            color: ["#C4EC1D"],
-            stagger: 0.2, // Each letter changes one by one
-            repeat: -1,
-            yoyo: true,
-            ease: "power1.inOut",
-            duration: 0.5
-        });
-    }, []);
-
-    const text = "Designer".split("").map((char, i) => (
-        <span key={i} className="inline-block">{char}</span>
-    ));
+  const text = "Designer".split("").map((char, i) => (
+    <span key={i} className="inline-block">{char}</span>
+  ));
 
   return (
-    <div className=' w-full  h-full overflow-hidden '>
-      <div className=' h-full     common common-padding'>
-<div className="flex flex-col gap-4 ">
-      <div className=' '>
-        <p className='text-[5vw] md:text-[2.5vw] flex items-center gap-2 text-white hover:text-[#C4EC1D] transition-all   '>Currently Availabe   For Freelance <br /> WorldWide </p>
-       
+    <div className="w-full overflow-hidden bg-[#0a0a0a]">
+      <div className="px-5 md:px-10 flex flex-col-reverse md:flex-row overflow-hidden items-center justify-between min-h-screen">
+        {/* Left Side Content */}
+        <div className="flex flex-col gap-6 w-full md:w-1/2 text-white">
+          {/* Availability Text */}
+          <p className="text-[5vw] md:text-[2.5vw] flex items-center gap-2 hover:text-[#C4EC1D] transition-all">
+            Currently Available For Freelance <br /> Worldwide 
+          </p>
+
+          {/* Hero Title */}
+          <h1 ref={textRef} className="text-[8vw] md:text-[5vw] font-bold leading-[0.9]">
+            Creative Visual <br /> {text}
+          </h1>
+
+          {/* Work Process Section */}
+          <div className="flex ml-10 items-center gap-6 mt-6">
+            <div className="loader "></div>
+            <p className="text-lg md:text-2xl leading-none ml-5">Work <br /> Process</p>
+          </div>
         </div>
-        {/* Hero-Title */}
-    <h1 ref={textRef} className='text-[8vw] text-white  font-bold leading-none'>Creative Visual <br /> {text}
-    </h1>
 
-
-</div>
-
-
+        {/* Right Side Profile Image */}
+        <div className="w-full md:w-1/2 flex justify-center">
+          <img 
+            src="HeroManProfile.png" 
+            height={50}
+            className="max-w-xs md:max-w-md lg:max-w-lg rounded-lg shadow-lg"
+            alt="Hero Profile"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
